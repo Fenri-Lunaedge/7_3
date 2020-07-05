d466aa93ec5997cc172f30e3448563749ebb9da6
Nohara <arindernadsfatec@gmail.com>
2020-07-05 20:12:39 -0300

    Solucao paliativa para o erro na hora de importar a folha de estilos

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Button, TextInput,  ScrollView} from 'react-native';
+
 
 export default function App() {
+
+  const [contacts, setContacts] = useState ([]);
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+
+
+useEffect(() => {
+   console.log(contacts);
+}, [contacts]);
+
+ // getName
+ const capturaName = (name) => {
+   setName(name)
+ };
+ 
+ // getPhone
+ const capturaPhone = (phone) => {
+  setPhone(phone)
+ };
+
+ // setName and setPhone in Contacts[]
+ const addContact = () => {
+    setName(name) //Contato identificado pelo nome (n tem id)
+    setContacts (contacts => [...contacts, name] )
+    setContactsd (contacts => [...contacts, phone] )
+    console.log(contacts);
+ };
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{padding:50}}>
+      <View style={{borderBottomColor:'black', borderBottomWidth:1, marginBottom:4, padding:2}}>
+      <TextInput placeholder="Name: " style={{borderBottomColor:'black', borderBottomWidth:1, marginBottom:4, padding:2}}/>
+          onChangeText={capturaName} value={name}/>
+        <TextInput placeholder="Phone: " style={{borderBottomColor:'black', borderBottomWidth:1, marginBottom:4, padding:2}}/>
+          onChangeText={capturaPhone} value={phone}/>
+        <Button title="+" onPress={addContact} />
+      </View>
+        <View>{
+          contacts.map((contacts) =><View><Text key={contacts}>{contacts}</Text></View>)
+        }
+      }
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  telaPrincipalView: {
+  padding: 50
   },
-});
+  contactView: {
+  // Definir aqui os valores se conseguir instalar o plugin
+},
+  contatoInputText: {
+  // Definir aqui os valores se conseguir instalar o plugin
+  }     
+  });
